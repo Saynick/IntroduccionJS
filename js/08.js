97dd468534d6f2f56a30c9c983f7a5e8049f6bc4
Saynick <oscarmartinezsalazar@gmail.com>
2022-06-20 17:54:05 -0500

    JS

--- js/08.js
+++ js/08.js
@@ -1,1 +1,16 @@
 //Orden de las Operaciones
+
+let resultado;
+resultado = 20 + 30 * 2;
+resultado = (100 + 200 + 300) * 1.16;
+//console.log(resultado);
+
+//incrementos
+let puntaje = 10;
+
+
+console.log(puntaje++);
+console.log(puntaje);
+
+
+
