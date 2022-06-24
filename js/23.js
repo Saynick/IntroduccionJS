22a685d6162ecc71648e902e39e362be6f01d84e
Saynick <oscarmartinezsalazar@gmail.com>
2022-06-24 14:23:49 -0500

    JS

---
+++ js/23.js
@@ -0,0 +1,25 @@
+/*  const puntaje = 1000;
+ if(puntaje !== 1000) 
+console.log('No es igual')
+ else {
+console.log('el puntaje es 1000');
+}
+ */
+/* const efectivo = 700;
+const carrito = 800;
+
+if( efectivo > carrito ) { //La cantidad de efectivo es mayor se puede pagar y se cumple la condición
+console.log('El Usuario puede pagar');
+} else {
+console.log('Fondos Insuficientes');
+}
+ */
+
+/* const rol = 'VISITANTE';
+
+if(rol === 'ADMINISTRADOR') {
+    console.log('Acceso a todo el sistema');
+} else if(rol === 'EDITOR') {
+    console.log('Eres editor, puedes entrar pero no puedes hacer mucho')
+} else 
+    console.log('No tienes acceso') */
